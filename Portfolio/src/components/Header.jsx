646b55ddceb2f2aec10e85af6e489d40cc92c43c
Igor Lima <igorlima7194@gmail.com>
2025-03-14 01:29:07 -0300

    header e a herosection

---
+++ Portfolio/src/components/Header.jsx
@@ -0,0 +1,23 @@
+
+import "../styles/header.css";
+
+import igorLogo from '../assets/logo.svg'; 
+
+
+const Header = () => {
+
+    return (
+        <div className="header">
+            <img src={igorLogo} alt="Logo do Igor" className="logo"/>
+            <ul className="navbar">
+                <li>Tecnologias</li>
+                <li>Projetos</li>
+                <li>Experiencia</li>
+                <li>Sobre Mim</li>
+            </ul>
+        </div>
+    )
+
+}
+
+export default Header;
